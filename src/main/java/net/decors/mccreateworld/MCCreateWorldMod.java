17442717150514a32df155d15f244c4b78257a7d
Decors <stealerofsoulz@gmail.com>
2023-07-15 17:26:33 +0900

    added a comment

---
+++ src/main/java/net/decors/mccreateworld/MCCreateWorldMod.java
@@ -0,0 +1,17 @@
+package net.decors.mccreateworld;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MCCreateWorldMod implements ModInitializer {
+    public static final String MOD_ID = "mscreateworld";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// new comment
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/decors/mccreateworld/MCCreateWorldModClient.java
@@ -0,0 +1,10 @@
+package net.decors.mccreateworld;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCCreateWorldModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
